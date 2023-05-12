61aeabb5845676f2f5aefaec335b76494524a180
nerdy-monk <akarshtripathi03@gmail.com>
2023-05-12 14:09:14 +0530

    admin panel navigation

--- src/layout/PrivateBody.js
+++ src/layout/PrivateBody.js
@@ -35,26 +35,28 @@ function PrivateNav(props) {
                     <div onClick={() => navigate('/', { replace: true })} className='logoLink'>
                         AgriTech Console
                     </div>
+                    {userData?.admin && < Button variant='success' className='admin-btn' onClick={() => navigate("/admin/panel")}>Admin Panel</Button>}
                 </div>
                 {/* <div className='d-flex justify-content-between pt-1'> */}
                 <div className='links-container'>
-                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/dashboard"><li className={`nav-element ${tab === "dashboard" ? "nav-element-selected" : ""}`}><img src = {dashboard} height = {45} width = {45}/>Dashboard</li></Link>
-                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/wallet"><li className={`nav-element ${tab === "wallet" ? "nav-element-selected" : ""}`}><img src = {wallet} height = {45} width = {45}/>Wallet</li></Link>
-                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/management"><li className={`nav-element ${tab === "management" ? "nav-element-selected" : ""}`}><img src = {management} height = {45} width = {45}/>Management</li></Link>
-                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/campaigns"><li className={`nav-element ${tab === "campaigns" ? "nav-element-selected" : ""}`}><img src = {campaigns} height = {45} width = {45}/>Campaigns</li></Link>
-                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/agristore"><li className={`nav-element ${tab === "agristore" ? "nav-element-selected" : ""}`}><img src = {agristore} height = {45} width = {45}/>AgriStore</li></Link>
+                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/dashboard"><li className={`nav-element ${tab === "dashboard" ? "nav-element-selected" : ""}`}><img src={dashboard} height={45} width={45} />Dashboard</li></Link>
+                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/wallet"><li className={`nav-element ${tab === "wallet" ? "nav-element-selected" : ""}`}><img src={wallet} height={45} width={45} />Wallet</li></Link>
+                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/management"><li className={`nav-element ${tab === "management" ? "nav-element-selected" : ""}`}><img src={management} height={45} width={45} />Management</li></Link>
+                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/campaigns"><li className={`nav-element ${tab === "campaigns" ? "nav-element-selected" : ""}`}><img src={campaigns} height={45} width={45} />Campaigns</li></Link>
+                    <Link onClick={() => setHamShow(false)} className={`link theme-${props.theme}`} to="/agristore"><li className={`nav-element ${tab === "agristore" ? "nav-element-selected" : ""}`}><img src={agristore} height={45} width={45} />AgriStore</li></Link>
                     <Button variant='btn bg-warning bg-opacity-75' onClick={() => logout()}>LOGOUT</Button>
                 </div>
                 {/* <div className='px-3'>
                         
                     </div> */}
-            {/* </div> */}
-        </div >
+                {/* </div> */}
+            </div >
 
             <div className={`private-nav d-block d-md-none theme-${props.theme}`}>
                 <div className='display-5 logoConsole'>AgriTech Console</div>
                 <div className='hamButtonNav'>
                     <FontAwesome name='bars' onClick={() => setHamShow(!hamShow)} className="fa-solid fa-bars" />
+                    {userData?.admin && < Button variant='success' className='admin-btn' onClick={() => navigate("/admin/panel")}>Admin Panel</Button>}
                     <Button variant='btn bg-warning bg-opacity-75' onClick={() => logout()}>LOGOUT</Button>
                 </div>
                 <div className={`dropDownNav ${hamShow ? 'showDropDownNav' : ''}`}>
